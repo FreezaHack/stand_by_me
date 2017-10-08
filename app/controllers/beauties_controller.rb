af6dd5908c6bf8a2242c9ba1247e9a123b5c3d83
Hironori Akaishi <ah.gm3622@gmail.com>
2017-10-08 09:49:04 +0900

    色々

--- app/controllers/beauties_controller.rb
+++ app/controllers/beauties_controller.rb
@@ -2,4 +2,22 @@ class BeautiesController < ApplicationController
   def index
     @beauties = Beauty.all
   end
+
+  def show
+    @beauties = Beauty.find(params[:id])
+    @time = WakeUp.find_by(user_id: current_user.id)
+  end
+
+  def time
+    @wakeup = WakeUp.new
+    @times = WakeUp.find_by(user_id: current_user.id)
+  end
+
+  def time_set
+    WakeUp.create(user_id: params[:user_id], wake_up_time: params[:wake_up][:wake_up_time], achievement: params[:achievement])
+    redirect_to wake_up_set_url
+
+  end
+
+
 end
